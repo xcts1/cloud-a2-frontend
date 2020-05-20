8cc55a356026aeebc3058fc0d6352f216dcecdf1
xcts1 <s3758834@rmit.edu.vn>
2020-05-20 08:40:24 +0700

    Building our React app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./App.css";
 import Routes from "./Routes";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
 
 function App() {
+  const history = useHistory();
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+  
+    userHasAuthenticated(false);
+    history.push("/login");
+  }
+
   return (
+    !isAuthenticating &&
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
@@ -17,16 +49,25 @@ function App() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+            {isAuthenticated
+              ? <NavItem onClick={handleLogout}>Logout</NavItem>
+              : <>
+                <LinkContainer to="/signup">
+                  <NavItem>Signup</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <NavItem>Login</NavItem>
+                </LinkContainer>
+              </>
+            }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider
+        value={{ isAuthenticated, userHasAuthenticated }}
+      >
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
-    s3: {
-      REGION: "ap-southeast-1",
-      BUCKET: "tqmnote"
-    },
-    apiGateway: {
-      REGION: "ap-southeast-1",
-      URL: "https://4awy7b5rac.execute-api.ap-southeast-1.amazonaws.com/dev"
-    },
-    cognito: {
-      REGION: "ap-southeast-1",
-      USER_POOL_ID: "ap-southeast-1_pxHXvnT42",
-      APP_CLIENT_ID: "ikbgahlouupfomd46n1a5unel",
-      IDENTITY_POOL_ID: "ap-southeast-1:7dc94df2-9b7f-4773-919c-201235b8f313"
-    }
-  };
+  MAX_ATTACHMENT_SIZE: 5000000,
+  
+  s3: {
+    REGION: "ap-southeast-1",
+    BUCKET: "tqmnote"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-1",
+    URL: "https://4awy7b5rac.execute-api.ap-southeast-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "ap-southeast-1",
+    USER_POOL_ID: "ap-southeast-1_pxHXvnT42",
+    APP_CLIENT_ID: "ikbgahlouupfomd46n1a5unel",
+    IDENTITY_POOL_ID: "ap-southeast-1:7dc94df2-9b7f-4773-919c-201235b8f313"
+  }
+};
